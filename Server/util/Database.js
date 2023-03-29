f213bff2cd14f0b90d89699f3e1417ffb70718e6
Florian Gericke <129275363+FlorianGericke@users.noreply.github.com>
2023-03-29 15:52:22 +0200

    Update Database.js

--- Server/util/Database.js
+++ Server/util/Database.js
@@ -3,7 +3,7 @@ const {Sequelize} = require('sequelize');
 let sequelize = new Sequelize('Assignment-todolists', 'root', 'password', {
     dialect: 'mysql',
     dialectOptions: {
-        socketPath: '/tmp/mysql.sock' //todo Wiso brauche ich diese Zeile ???
+        socketPath: '/tmp/mysql.sock' 
     },
     host: 'localhost',
 });
